33f05a55d63fbf8608c8303a98b1c398de11b49e
Simon Nordberg <simon@simonnordberg.com>
2023-10-12 14:25:50 +0200

    Fw stuff

--- hcloud.tf
+++ hcloud.tf
@@ -13,26 +13,27 @@ provider "hcloud" {
 
 # Servers
 resource "hcloud_server" "photoprism" {
-  name        = "photoprism-server"
-  image       = "ubuntu-22.04"
-  server_type = "cx11"
-  location    = var.location
-  ssh_keys    = [hcloud_ssh_key.default.id]
-  labels      = {
+  name         = "photoprism-server"
+  image        = "ubuntu-22.04"
+  server_type  = "cx11"
+  location     = var.location
+  ssh_keys     = [hcloud_ssh_key.default.id]
+  firewall_ids = [hcloud_firewall.photoprism_firewall.id]
+  labels       = {
     type = "photoprism"
   }
   user_data = file("setup/ubuntu/user_data.yml")
 }
 
 # Volumes
-resource "hcloud_volume" "photoprism_data" {
-  name      = "photoprism-data"
-  server_id = hcloud_server.photoprism.id
-  size      = 10
-  format    = "ext4"
-  automount = true
-  depends_on = [hcloud_server.photoprism]
-}
+#resource "hcloud_volume" "photoprism_data" {
+#  name      = "photoprism-data"
+#  server_id = hcloud_server.photoprism.id
+#  size      = 10
+#  format    = "ext4"
+#  automount = true
+#  depends_on = [hcloud_server.photoprism]
+#}
 
 #resource "hcloud_volume" "photoprism_import" {
 #  name      = "photoprism-import"
@@ -45,6 +46,26 @@ resource "hcloud_volume" "photoprism_data" {
 
 resource "hcloud_firewall" "photoprism_firewall" {
   name = "photoprism-firewall"
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "22"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
 }
 
 
